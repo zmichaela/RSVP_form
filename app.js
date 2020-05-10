aa2f9111544e2f0dff5bcb91b4a3800724717b25
m.zajacova90@gmail.com <Sestra90@>
2020-05-10 13:25:27 +0100

    added edit button
    edit button to edit names, updated event handler to handle edit and remove clicks

--- app.js
+++ app.js
@@ -6,32 +6,36 @@ const ul = document.getElementById("invitedList");
 
 //create li functionwith text as a parameter
 function createLi (text) {
-    const li = document.createElement("li");
-      li.textContent = text;
-      //added confirmed checkbox
-      const label = document.createElement("label");
-      label.textContent = "Confirmed";
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      label.appendChild(checkbox);
-      li.appendChild(label);
-      //added remove button
-      const button = document.createElement("button");
-      button.textContent = "remove";
-      li.appendChild(button);
-      return li;
-    }
-    
-    
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const text = input.value;
-      input.value = "";
-      const li = createLi (text);
-      ul.appendChild(li);
-    })
+const li = document.createElement("li");
+  li.textContent = text;
+  //added confirmed checkbox
+  const label = document.createElement("label");
+  label.textContent = "Confirmed";
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  label.appendChild(checkbox);
+  li.appendChild(label);
+  //
+  const editButton = document.createElement("button");
+  editButton.textContent = "edit";
+  li.appendChild(editButton);
+  //added remove button
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "remove";
+  li.appendChild(removeButton);
+  return li;
+}
 
-    
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const text = input.value;
+  input.value = "";
+  const li = createLi (text);
+  ul.appendChild(li);
+})
+
+
 
 ul.addEventListener("change", (e) => {
   const checkbox = event.target;
@@ -46,17 +50,23 @@ ul.addEventListener("change", (e) => {
 });
 
 
+// remove and edit names - event handlers
 ul.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
+    const button = e.target;
     const li = e.target.parentNode;
     const ul = li.parentNode;
-    ul.removeChild(li);
+    if (button === "remove") {
+      ul.removeChild(li);
+    } else if (button === "edit") {
+      console.log("edit");
+    }
   } 
 })
 
 
 
-
+//edit names and save
 
 
 
