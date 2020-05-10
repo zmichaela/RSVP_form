645c4f8e1d2d5b77a4e090c2028f54c9a2411340
m.zajacova90@gmail.com <Sestra90@>
2020-05-10 10:46:43 +0100

    added checkbox

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const form = document.getElementById("registrar");
 const input = form.querySelector("input");
+const ul = document.getElementById("invitedList");
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const text = input.value;
   input.value = "";
-  const ul = document.getElementById("invitedList");
   const li = document.createElement("li");
   li.textContent = text;
   const label = document.createElement("label");
@@ -16,3 +17,17 @@ form.addEventListener("submit", (e) => {
   li.appendChild(label);
   ul.appendChild(li);
 })
+
+
+ul.addEventListener("change", (e) => {
+  const checkbox = event.target;
+  const checked = checkbox.checked;
+  const listItem = checkbox.parentNode.parentNode;
+  if (checked) {
+    listItem.className = "responded";
+  }
+  else {
+    listItem.className = "";  
+  }
+});
+
